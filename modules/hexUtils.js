8a593a9b32ccaacb969c462db6f41b83fd84b940
Jon Bennett <jdbennet@ca.ibm.com>
2017-01-11 12:46:30 -0500

    Add internal key routine

--- modules/hexUtils.js
+++ modules/hexUtils.js
@@ -2,8 +2,20 @@
   Provide path / analysis models for the current state of the board
  */
 
+var _ = require('lodash');
+
 function hexUtils(state){
 
+}
+
+hexUtils.prototype.adjacent = function(x,y){
+
+};
+
+function key( x, y ){
+    x = _.padStart(x.toString(), 2, '0');
+    y = _.padStart(y.toString(), 2, '0');
+    return x + ' - ' + y;
 }
 
 module.exports = hexUtils;
